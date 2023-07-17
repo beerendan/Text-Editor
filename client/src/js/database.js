32dd5f720edd3533ccf492314ae514b2265f82c6
Beerendan <brendanrbowen@gmail.com>
2023-07-16 20:20:33 -0400

    added package scripts

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {console.log('putDb implemented');
+export const putDb = async (id, content) => {console.log('putDb implemented');
   const jateDB=await openDB('jate',1);
   const tx=jateDB.transaction('jate','readwrite');
   const store=tx.objectStore('jate');
-  const request=store.put({id:1, value:content});
+  const request=store.put({id:id, value:content});
   const result=await request;
   console.log('🚀 - data saved to the database', result);
 
